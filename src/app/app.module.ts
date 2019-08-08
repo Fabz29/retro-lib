63c6a0fa13935f1c2b00cbd168ad53f3d7e94fc1
Fabien29 <fzanetti@intimesit.com>
2019-08-08 09:16:38 +0200

    Start first page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule, MatSidenavModule, MatToolbarModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
