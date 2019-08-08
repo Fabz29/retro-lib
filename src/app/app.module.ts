6cac03ee6bb13cc5dcd859722c62dbc7f8277717
Fabien29 <fzanetti@intimesit.com>
2019-08-08 10:03:24 +0200

    Make architecture.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCardModule, MatSidenavModule, MatToolbarModule, MatIconModule} from '@angular/material';
+import {MatCardModule, MatToolbarModule, MatIconModule} from '@angular/material';
+import {SigninComponent} from './auth/signin/signin.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {GameListComponent} from './game-list/game-list.component';
+import {SingleGameComponent} from './game-list/single-game/single-game.component';
+import {GameFormComponent} from './game-list/game-form/game-form.component';
+import {HeaderComponent} from './header/header.component';
+import {AuthService} from './services/auth/auth.service';
+import {AuthGuardService} from './services/auth/auth-guard.service';
+import {GamesService} from './services/game/games.service';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'auth/signup', component: SignupComponent},
+  {path: 'auth/signin', component: SigninComponent},
+  {path: 'games', component: GameListComponent},
+  {path: 'games/new', component: GameFormComponent},
+  {path: 'games/view/:id', component: SingleGameComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    GameListComponent,
+    SingleGameComponent,
+    GameFormComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
     MatToolbarModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuardService,
+    GamesService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/models/game.model.ts
@@ -0,0 +1,7 @@
+export class Book {
+  private photo: string;
+  private description: string;
+
+  constructor(private title: string, private console: string) {
+  }
+}
